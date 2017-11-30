3a350d1f9cde6954db4f9adb98142c2a891b151e
kigesui <kigesui@hotmail.com>
2017-11-30 14:59:22 -0500

    using embed helper in eat

--- modules/eat/eat.py
+++ modules/eat/eat.py
@@ -1,9 +1,8 @@
 from ..i_module import IModule, ExecResp
 from utils.bot_config import BotConfig
+from utils.bot_embed_helper import EmbedHelper
 import random
 import re
-
-from discord import Embed
 
 
 class EatModule(IModule):
@@ -17,15 +16,13 @@ class EatModule(IModule):
         command = cmd_args[0]
         if command == "eat":
             if not re.match("^eat$", cmd):
-                embed = Embed()
-                embed.colour = BotConfig().get_hex("Colors", "OnError")
-                embed.description = "Usage: {}eat".format(
-                                    BotConfig().get_botprefix())
+                msg = "Usage: {}eat".format(
+                      BotConfig().get_botprefix())
+                embed = EmbedHelper.error(msg)
                 return [ExecResp(code=500, args=embed)]
 
             choice = random.choice(self.__list)
-            embed = Embed()
-            embed.colour = BotConfig().get_hex("Colors", "OnSuccess")
+            embed = EmbedHelper.success()
             embed.description = str(choice)
             return [ExecResp(code=200, args=embed)]
 
